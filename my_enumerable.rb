8aab594fcd2ba0735d92f1e7eef2df41c11695c2
Caleb Anderson <buyananderson@gmail.com>
2021-10-08 10:38:13 +0200

    Create the all function

---
+++ my_enumerable.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# a extension for the list object, to be able to make use of it's each function
+module MyEnumerable
+  def all?
+    each do |e|
+      return false unless yield e
+    end
+    true
+  end
+
+
